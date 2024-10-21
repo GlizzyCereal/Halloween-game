990859049b8b001f7c8e1f875424613863ee2130
GlizzyCereal <haroldas.bogdanovicius@gmail.com>
2024-10-21 18:26:01 +0300

    Added player and player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float move = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(move, 0, 0);
     }
 }
